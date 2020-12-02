71438cc8b49796cc2541bac4d273a7e3070c0c4a
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-12-02 20:11:05 +0100

    Hello, Console! implemented

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-name = "World"
-print(f'Hello, {name}!')
+import sys
+
+
+def main():
+    name = "World"
+    if len(sys.argv) == 1:
+        print(f'Hello, {name}!')
+    else:
+        name = " "
+        arguments = sys.argv[1:]
+        name = name.join(arguments)
+        print(f'Hello, {name}!')
+
+
+if __name__ == '__main__':
+    main()
